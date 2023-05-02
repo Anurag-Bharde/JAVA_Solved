5292f9bf39799e2f6b2aabe54af1065899a599d4
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-05-02 23:16:33 +0530

    add eist

---
+++ rot.java
@@ -0,0 +1,13 @@
+public class rot{
+    public static void main(String[] args) {
+        int ar[]={1,2,4,5,6,7};
+        int n=3;
+        int t=0;
+        for(int i=n;i<ar.length;i++){
+           ar[i]=(n-1)%n;
+        }
+        for(int i=0;i<ar.length;i++){
+           System.out.println(ar[i]);
+        }
+    }
+}
